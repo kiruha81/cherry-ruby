734351ccf5e2ad0de4ff5384143f22efe3be256f
EC2 Default User <ec2-user@ip-172-31-4-186.ap-northeast-1.compute.internal>
2023-01-10 06:19:30 +0000

    [Finish]2

---
+++ calc.rb
@@ -0,0 +1,30 @@
+puts 10 + 20
+puts 100 - 25
+puts 12 * 5
+puts 20 / 5
+
+n = 1
+puts -n * 5
+puts n.to_f
+puts n.to_f / 2
+puts 1 / 2
+puts 1.0 / 2
+puts 1 / 2.0
+puts 8 % 3
+puts 2 ** 3
+puts 1 < 2
+puts 1 <= 2
+puts 1 > 2
+puts 1 >= 2
+puts 1 == 2
+puts 1 == 1
+puts 1 != 2
+puts 2 * 3 + 4 * 5 - 6 / 2
+puts 2 * (3 + 4) * (5 - 6) / 2
+puts n += 1
+puts n -= 1
+
+x = 2
+puts x *= 3
+puts x /= 2
+puts x **= 2
